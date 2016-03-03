94b67af8b69bddb16f87cd7fc27f24e83383f4a6
kuzmiand <rabota.kuzmiand@gmail.com>
2016-03-03 07:03:28 +0100

    siteController

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -108,8 +108,8 @@ class SiteController extends Controller
 
         $b_array = [10, 20, 30, 40];
         $c = 51;
-        //$d_array = [201415, 201314, 201213, 201112, 201011];
-        $d_array = [201415];
+        $d_array = [201415, 201314, 201213, 201112, 201011];
+        //$d_array = [201314];
 
         foreach($d_array as $d){
 
@@ -265,59 +265,88 @@ class SiteController extends Controller
 
         $country = DrawCountry::find()->where(['id'=>1])->one();
         $league = DrawLeague::find()->where(['name'=>'D1'])->one();
-        $season = DrawSeason::find()->where(['slug'=>'201415'])->one();
 
-        $league_season = null;
-        if($country && $league && $season){
-            $league_season = DrawRelLeagueSeason::find()->where(['league_id'=>$league->id, 'season_id'=>$season->id, 'country_id'=>$country->id])->one();
-        }
+        $seasons = DrawSeason::find()->where(['or', ['slug'=> [201415, 201314, 201213, 201112, 201011]]])->all();
 
-        if($league_season){
-            $rel_team_season = DrawRelTeamSeason::find()->where(['season_league_id'=>$league_season->id])->all();
-            if(!empty($rel_team_season)){
-                $win_sum_all = 0;
-                $all_pay_sum_all = 0;
-                foreach($rel_team_season as $key=>$team_season){
-                    $team = DrawTeam::find()->where(['id'=>$team_season->team_id])->one();
-                    $games_team = DrawGame::find()->where(['and',['season_league_id'=>$league_season->id],['or',['team_home_id'=>$team->id],['team_guest_id'=>$team->id]]])->all();
-
-                    if($games_team){
-                        $start_sum = 1;
-                        $win_sum = 0;
-                        $all_pay_sum = 0;
-                        foreach($games_team as $kk=>$game){
-
-
-                           /* if($kk>=18){
-                                break;
-                            }*/
-                            if($game->team_home_goals != $game->team_guest_goals ){
-                                $all_pay_sum = $all_pay_sum+$start_sum;
-                                $start_sum = $start_sum*2;
-
-                            }else{
-                                $win_sum = $win_sum+($start_sum*3);
-                                $all_pay_sum = $all_pay_sum+$start_sum;
-                                $start_sum = 1;
-                            }
-
-                        }
-                        $win_sum_all = $win_sum_all+$win_sum;
-                        $all_pay_sum_all = $all_pay_sum_all+$all_pay_sum;
-                        echo "<pre>";
-                        var_dump($team->name);
-                        var_dump($all_pay_sum, $win_sum);
-                        echo "----------------------------------------------------------------------------------------";
-                        echo "</pre>";
-                    }
-                }
-                echo "<pre>";
-                var_dump($all_pay_sum_all,$win_sum_all);
-                echo "</pre>";
-                die();
+        foreach($seasons as $season){
+            $league_season = null;
+            if($country && $league && $season){
+                $league_season = DrawRelLeagueSeason::find()->where(['league_id'=>$league->id, 'season_id'=>$season->id, 'country_id'=>$country->id])->one();
             }
 
+            if($league_season){
+                $rel_team_season = DrawRelTeamSeason::find()->where(['season_league_id'=>$league_season->id])->all();
+                if(!empty($rel_team_season)){
+                    $win_sum_all = 0;
+                    $all_pay_sum_all = 0;
+                    foreach($rel_team_season as $key=>$team_season){
+                        $team = DrawTeam::find()->where(['id'=>$team_season->team_id])->one();
+                        $games_team = DrawGame::find()->where(['and',['season_league_id'=>$league_season->id],['or',['team_home_id'=>$team->id],['team_guest_id'=>$team->id]]])->all();
+
+                        if($games_team){
+                            $start_sum = 1;
+                            $win_sum = 0;
+                            $all_pay_sum = 0;
+
+                            $not_draw =0;
+                            $dr = 0;
+                            foreach($games_team as $kk=>$game){
+
+
+                               /* if($kk<=9 || $kk>=29){
+                                    continue;
+                                }*/
+
+                                if($game->team_home_goals != $game->team_guest_goals ){
+                                    $all_pay_sum = $all_pay_sum+$start_sum;
+                                    //$start_sum = $start_sum*2;
+
+                                    if($dr){
+                                        $dr=0;
+                                        $not_draw = 1;
+                                        $start_sum = $start_sum*2;
+                                    }else{
+                                        $not_draw++;
+                                        if($not_draw == 10){
+                                            $not_draw = 0;
+                                            $start_sum = 1;
+                                        }else{
+                                            $start_sum = $start_sum*2;
+                                        }
+                                    }
+
+
+
+                                }else{
+                                    $dr=1;
+                                    $win_sum = $win_sum+($start_sum*(4));
+                                    $all_pay_sum = $all_pay_sum+$start_sum;
+                                    $start_sum = 1;
+                                }
+
+                            }
+                            $win_sum_all = $win_sum_all+$win_sum;
+                            $all_pay_sum_all = $all_pay_sum_all+$all_pay_sum;
+                            echo "<pre>";
+                            var_dump($team->name);
+                            var_dump($all_pay_sum, $win_sum);
+                            echo "----------------------------------------------------------------------------------------";
+                            echo "</pre>";
+                        }
+                    }
+                    echo "----------------------------------------------------------------------------------------";
+
+                    echo "<pre>";
+                    echo $season->slug.'<br>';
+                    var_dump($all_pay_sum_all,$win_sum_all);
+                    echo "</pre>";
+                    echo "----------------------------------------------------------------------------------------";
+                }
+
+            }
         }
+
+
     }
 
 }
